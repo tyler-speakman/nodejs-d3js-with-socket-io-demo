c45769f47beb10f1d230a87fb3e6258d36a8a0b7
Tyler Speakman <tyler.speakman@gmail.com>
2014-05-12 16:35:39 -0400

    - Initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+<p>Run:</p>
+
+<blockquote>npm start</blockquote>
+
+<p>Navigate to:</p>
+
+<blockquote><a href="http://localhost:99/">http://localhost:99/</a></blockquote>
---
+++ server.js
@@ -0,0 +1,49 @@
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app), //require('socket.io').listen(80);
+    fs = require('fs');
+
+app.listen(99);
+
+function handler(req, res) {
+    console.log('handler')
+    fs.readFile(__dirname + '/index.html',
+        function(err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+// Manage connections
+io.sockets.on('connection', function(socket) {
+    console.log('handle connection');
+
+    var periodInMilliseconds = 100;
+    var timeoutId = -1;
+
+    //
+    var handleDisconnect = function() {
+        console.log('handle disconnect');
+
+        clearTimeout(timeoutId);
+    };
+
+    var generateServerRequest = function() {
+        console.log('generate server request');
+
+        socket.emit('server request', {
+            date: new Date(),
+            value: Math.random()
+        });
+
+        timeoutId = setTimeout(generateServerRequest, periodInMilliseconds);
+    };
+
+    socket.on('disconnect', handleDisconnect);
+
+    timeoutId = setTimeout(generateServerRequest, periodInMilliseconds);
+});
